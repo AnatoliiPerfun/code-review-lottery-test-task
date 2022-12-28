0e7d725958d2569483c4fa0b1ac215cf2fa2c08e
Anatolii Perfun <54056693+AnatoliiPerfun@users.noreply.github.com>
2022-12-29 00:00:04 +0200

    Update ColorSupplier.java

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-
-
+    //hear two empty lines whatsoever it is not crustal
     public static String getRandomColor() {
         int index = new Random().nextInt(Colors.values().length);
-        return Colors.values()[index].toString();
+        return Colors.values()[index].getColor(); // remove to String as we pass enums as Strings already.
     }
 }
