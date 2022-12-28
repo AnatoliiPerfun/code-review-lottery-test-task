5eefc8710a63746d8897191b6ceb196a0b552b26
Anatolii Perfun <54056693+AnatoliiPerfun@users.noreply.github.com>
2022-12-29 00:00:30 +0200

    Update Colors.java

--- src/main/java/core/basesyntax/Colors.java
+++ src/main/java/core/basesyntax/Colors.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
 public enum Colors {
-    black,
-    white,
-    red,
-    blue
+      black,
+      white,
+      red,
+      blue;
+    
+      // this method returns a String object and with the name() method we get the name of the color
+      public String getColor() {
+            return this.name();
+      }
 }
